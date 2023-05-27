409aa0e5ddd882dc1345e75b30ec5e91efdda91d
Shehan Maxwell <shehanbbn@gmail.com>
2023-05-27 22:40:38 +0530

    components test

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,1 +1,9 @@
- 
+import { Component } from 'react';
+
+class CardList extends Component {
+  render() {
+    return <div>Hello im the card list Component</div>;
+  }
+}
+
+export default CardList;
